15f1f3b6350a944ac13581905916db939c4d4f27
forrestkirby <18228336+forrestkirby@users.noreply.github.com>
2021-04-01 14:22:37 +0200

    Delete content.php

--- herzogdupont/includes/modules/elements/elements/hd-counter/templates/content.php
+++
@@ -1,5 +0,0 @@
-<?php if ($props['number']) : ?>
-<div>
-	<?= $props['number'] ?><?= $props['unit'] ? ' ' . $props['unit'] : '' ?><?= $props['text'] ? ' ' . $props['text'] : '' ?>
-</div>
-<?php endif ?>
